f81a0597b89bcf8c1f56c01d995e0d2a0f9db895
Walvy666 <businesswalvy@gmail.com>
2026-03-04 13:32:24 +0700

    Update Nine_Elephent.lua

--- Nine_Elephent.lua
+++ Nine_Elephent.lua
@@ -1,3 +1,8 @@
-local char = character:WaitForChild("Humanoid")
-char:SetStateEnabled(Enum.HumanoidStateType.FallingDown,false)
-char:SetStateEnabled(Enum.HumanoidStateType.Ragdoll,false)
+repeat task.wait() until game.Players.LocalPlayer.Character
+
+local plr = game.Players.LocalPlayer
+local char = plr.Character or plr.CharacterAdded:Wait()
+local hum = char:WaitForChild("Humanoid")
+
+hum:SetStateEnabled(Enum.HumanoidStateType.FallingDown,false)
+hum:SetStateEnabled(Enum.HumanoidStateType.Ragdoll,false)
