75bba8e8145d72ef6f29865dd5478b557dbd02e8
Walvy666 <businesswalvy@gmail.com>
2026-03-04 17:47:46 +0700

    Map

---
+++ ahhhshit.lua
@@ -0,0 +1,18 @@
+return {
+    Carrot = "Common",
+    Corn = "Common",
+    Onion = "Uncommon",
+    Strawberry = "Uncommon",
+    Mushroom = "Uncommon",
+    Beetroot = "Rare",
+    Tomato = "Rare",
+    Rose = "Rare",
+    Wheat = "Epic",
+    Banana = "Epic",
+    Plum = "Epic",
+    Potato = "Epic",
+    Cabbage = "Legendary",
+    Cherry = "Legendary",
+    Bamboo = "Legendary",
+    Mango = "Legendary"
+}
